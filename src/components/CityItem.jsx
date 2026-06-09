5831c625a3875e04da51bbddafbb87b3b52cdef7
mohammed-abdelwhab <mohamedabdelwhab197@gmail.com>
2026-06-10 00:08:10 +0300

    fix(static-layout) : re-format the date comming from the api

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,11 +1,22 @@
 import styles from "./CityItem.module.css";
 function CityItem({ city }) {
   const { cityName, emoji, date } = city;
+  const dateFormat = new Date(date);
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+
   return (
     <li className={styles.cityItem}>
       <span className={styles.emoji}>{emoji}</span>
       <p className={styles.name}>{cityName}</p>
-      <p className={styles.date}>{date}</p>
+      <p className={styles.date}>
+        {dateFormat.toLocaleDateString("en-US", options)}
+      </p>
       <button className={styles.deleteBtn}>&times;</button>
     </li>
   );
