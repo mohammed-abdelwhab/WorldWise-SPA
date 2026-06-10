87697a143975398ab6cd37758fecd185f2c99fee
mohammed-abdelwhab <mohamedabdelwhab197@gmail.com>
2026-06-10 18:45:13 +0300

    refactor(static-layout): Changed the index route using the Navigate component from react-router-dom to open the cities path directly

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import Login from "./pages/Login";
@@ -38,10 +38,7 @@ function App() {
         <Routes>
           <Route index element={<Homepage />} />
           <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              element={<CitiesList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to="cities" />} />
             <Route
               path="cities"
               element={<CitiesList cities={cities} isLoading={isLoading} />}
