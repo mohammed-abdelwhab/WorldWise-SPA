393523b102cc2988df77bc01734e0d1166f44804
mohammed-abdelwhab <mohamedabdelwhab197@gmail.com>
2026-06-10 18:32:40 +0300

    feat(re-usability):made a re-usable button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,13 @@
 import styles from "./Button.module.css";
-function Button() {
-  return <button className={styles.btn}>use your position</button>;
+// A re-usable component
+function Button({ children, variant, onClick }) {
+  //* adding the variant choosen by user to the style --> The || operator for validation if the user entered unvalid variant that doesn't exist in the style sheet.
+  const buttonClass = `${styles.btn} ${styles[variant] || ""}`.trim();
+  return (
+    <button onClick={onClick} className={buttonClass}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
